5e83b80ec487c849bb68251aac1075e74341df71
Sakshi Sangwan <sangwansakshi10@gmail.com>
2018-07-27 12:03:45 +0530

    Added file struct-pointers.go

---
+++ src/goprograms/MoreTypes/struct-pointers.go
@@ -0,0 +1,18 @@
+// To access fields of a struct, instead of (*p).X we can simply write p.X
+
+
+package main 
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+}
