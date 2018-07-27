fbf442a3d5b247e610e22351d65d0ea8784bbcff
Sakshi Sangwan <sangwansakshi10@gmail.com>
2018-07-27 12:51:42 +0530

    Added file slices-pointers.go

---
+++ src/goprograms/MoreTypes/slices-pointers.go
@@ -0,0 +1,26 @@
+// Slice does not store any data, it describes a section of underlying array
+// Changing elements of slice reflects into other slices and corresponding array
+
+
+package main 
+
+import "fmt"
+
+func main() {
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
