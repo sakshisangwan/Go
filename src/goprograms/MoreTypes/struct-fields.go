5762823243e62a24c1aef5121bd06794fb82620d
Sakshi Sangwan <sangwansakshi10@gmail.com>
2018-07-27 11:53:41 +0530

    Added file struct-fields.go

---
+++ src/goprograms/MoreTypes/struct-fields.go
@@ -0,0 +1,17 @@
+// Struct fields are accessed using a dor
+
+
+package main 
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+}
