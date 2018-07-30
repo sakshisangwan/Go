383a34d572f262b4ba600c7f7319c29faaeef816
Sakshi Sangwan <sangwansakshi10@gmail.com>
2018-07-30 11:31:04 +0530

    Added file range.go

---
+++ src/goprograms/MoreTypes/range.go
@@ -0,0 +1,15 @@
+// Ranging over a slice returns 2 values for each iteration.
+// The index and the copy of element at that index
+
+
+package main 
+
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
